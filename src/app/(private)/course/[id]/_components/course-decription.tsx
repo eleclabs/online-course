8d0f0128e02cdd5e6ebb4a268da8e109f170c853
eleclabs <eleclabs@gmail.com>
2025-02-14 06:24:43 +0700

    update

--- src/app/(private)/course/[id]/_components/course-decription.tsx
+++ src/app/(private)/course/[id]/_components/course-decription.tsx
@@ -2,6 +2,9 @@ import { ICourse } from "@/interfaces";
 import React from "react";
 import { Parser } from "html-to-react";
 
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 const parser = Parser();
 
 function CourseDescription({ course }: { course: ICourse }) {
@@ -9,7 +12,9 @@ function CourseDescription({ course }: { course: ICourse }) {
     <div>
       <h1 className="text-xl font-bold mt-5">Description</h1>
       <div className="mt-2 border bg-gray-100 border-primary p-5">
-        <div className="text-sm">{parser.parse(course.description)}</div>
+         {/* <div className="text-sm">{parser.parse(course.description)}</div>  */}
+         
+         <div dangerouslySetInnerHTML={{ __html: course.description }} />
       </div>
     </div>
   );
